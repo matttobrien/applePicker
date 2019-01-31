6c18b43b04e528d46df04b4a1c149d11f50df050
Matthew O'Brien <44552583+matttobrien@users.noreply.github.com>
2019-01-31 11:34:21 -0500

    Add files via upload

---
+++ HighScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){
+        //if there already is a player highscore read it
+        if(PlayerPrefs.HasKey("HighScore")){
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        //assigns the value of score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        //updates the Highscore if the players score is greater
+        if(score > PlayerPrefs.GetInt("HighScore")){
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
